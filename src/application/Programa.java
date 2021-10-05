4b802fdc922cb8ce156316a7b3662fc3e5c5dbb9
Iran Portela <iranportela@gmail.com>
2021-10-04 22:45:13 -0300

    Position criada

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -2,10 +2,15 @@ package application;
 
 import java.util.Locale;
 
+import boardgame.Position;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
+		
+		Position pos = new Position(6, 8);
+		System.out.println(pos);
 		
 		
 		
